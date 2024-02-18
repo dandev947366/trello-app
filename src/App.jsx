c8383e32bf3017b23a913fe26c3acef725cce90d
dandev947366 <dan.dev947366@gmail.com>
2024-02-19 00:14:30 +0200

    system mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,54 @@
 import { useState } from 'react'
 import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 
+
+//mode: dark - light - system
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+       
+        <MenuItem value='light'>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+          <LightModeIcon fontSize='small'/> Light
+          </div>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px'  }}>
+          <DarkModeOutlinedIcon fontSize='small'/> Dark
+          </div>
+        </MenuItem>
+        <MenuItem value='system'>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px'  }}>
+          <SettingsBrightnessIcon fontSize='small'/> System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
@@ -13,7 +60,7 @@ function ModeToggle() {
     >
       {mode === 'light' ? 'Turn dark' : 'Turn light'}
     </button>
-  );
+  )
 }
 
 function App() {
@@ -21,7 +68,11 @@ function App() {
 
   return (
     <>
+    <ModeSelect />
+    <hr />
     <ModeToggle />
+    <hr />
+    
       <div>Dan Le</div>
       <Typography variant='body2' color='text.secondary'>Test Typography</Typography>
       
