415361655423c504389f00d29752084d6852e8e4
dandev947366 <dan.dev947366@gmail.com>
2024-02-18 20:24:47 +0200

    CssVarsProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
 
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </button>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <ModeToggle />
       <div>Dan Le</div>
       <Typography variant='body2' color='text.secondary'>Test Typography</Typography>
       
